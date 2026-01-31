e00798bedcc255f6acbfd89283a995586aecb919
devpedroschuster <devpedroschuster@gmail.com>
2026-01-31 10:52:38 -0300

    Force deploy com hardcode

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import emailjs from '@emailjs/browser';
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+const RECAPTCHA_SITE_KEY = '6LexHFUsAAAAALR2NZ9fFYRFwzb4qiw69kcLiQJZ';
 
 emailjs.init(PUBLIC_KEY);
 
